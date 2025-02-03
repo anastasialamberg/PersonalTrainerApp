06a6e879b626e0d3b7fd0e5ec19a45a20b1788d9
anastasialamberg <anastasia.lamberg@gmail.com>
2025-02-03 20:11:35 +0200

    Updated api links

--- src/components/Customer.jsx
+++ src/components/Customer.jsx
@@ -1,160 +1,171 @@
 import { AgGridReact } from "ag-grid-react";
 import { useEffect, useState } from "react";
-import 'ag-grid-community/styles/ag-grid.css';
-import 'ag-grid-community/styles/ag-theme-material.css';
+import "ag-grid-community/styles/ag-grid.css";
+import "ag-grid-community/styles/ag-theme-material.css";
 import { Button, Snackbar } from "@mui/material";
 import AddCustomer from "./AddCustomer";
 import EditCustomer from "./EditCustomer";
 import CsvDownloader from "react-csv-downloader";
-
+import { get } from "lodash";
 
 export default function Customer() {
+  //States
+  const [customers, setCustomers] = useState([]);
+  const [openSnackbar, setOpenSnackBar] = useState(false);
+  const [snackmessage, setSnackMessage] = useState("");
 
+  //Kentät aggridiin
+  const columnDefs = [
+    { field: "firstname", sortable: true, filter: true, floatiFilter: true },
+    { field: "lastname", sortable: true, filter: true, floatiFilter: true },
+    {
+      field: "streetaddress",
+      sortable: true,
+      filter: true,
+      floatiFilter: true,
+    },
+    { field: "postcode" },
+    { field: "city", sortable: true, filter: true, floatiFilter: true },
+    { field: "email", sortable: true, filter: true, floatiFilter: true },
+    { field: "phone", sortable: true, filter: true, floatiFilter: true },
+    {
+      //cellrenderer poisto napille
+      headerName: "",
+      cellRenderer: (params) => (
+        <Button
+          size="small"
+          color="error"
+          onClick={() => deleteCustomer(params.data)}
+        >
+          Delete
+        </Button>
+      ),
+      width: 120,
+    },
+    //cellrenderer edit napille
+    {
+      headerName: "",
+      cellRenderer: (params) => (
+        <EditCustomer
+          customer={params.data}
+          onSave={updateCustomer}
+          onCancel={handleCancel}
+        />
+      ),
+      width: 120,
+    },
+  ];
 
-    //States
-    const [customers, setCustomers] = useState([]);
-    const [openSnackbar, setOpenSnackBar] = useState(false);
-    const [snackmessage, setSnackMessage] = useState("");
+  useEffect(() => {
+    getCustomers();
+  }, []);
 
-    //Kentät aggridiin
-    const columnDefs = [
-        { field: 'firstname', sortable: true, filter: true, floatiFilter: true },
-        { field: 'lastname', sortable: true, filter: true, floatiFilter: true },
-        { field: 'streetaddress', sortable: true, filter: true, floatiFilter: true },
-        { field: 'postcode' },
-        { field: 'city', sortable: true, filter: true, floatiFilter: true },
-        { field: 'email', sortable: true, filter: true, floatiFilter: true },
-        { field: 'phone', sortable: true, filter: true, floatiFilter: true },
-        {
-            //cellrenderer poisto napille
-            headerName: '',
-            cellRenderer: params => (
-                <Button
-                    size="small"
-                    color="error"
-                    onClick={() => deleteCustomer(params.data)}
-                >
-                    Delete
-                </Button>
-            ),
-            width: 120
+  //haetaan kaikki customerit, fetch
+  const getCustomers = () => {
+    fetch(
+      "https://customer-rest-service-frontend-personaltrainer.2.rahtiapp.fi/api/customers"
+    )
+      .then((response) => response.json())
+      .then((responseData) => {
+        setCustomers(responseData._embedded.customers);
+        console.log(responseData._embedded.customers);
+      })
+      .catch((error) => console.error(error));
+  };
+
+  //poistetaan customer
+  const deleteCustomer = (customer) => {
+    if (window.confirm("Are you sure you want to delete this customer?")) {
+      fetch(customer._links.customer.href, { method: "DELETE" })
+        .then((response) => {
+          if (response.ok) {
+            setSnackMessage("The customer was deleted succesfully!");
+            setOpenSnackBar(true);
+            getCustomers();
+          } else {
+            window.alert("Something went wrong with deleting");
+          }
+        })
+        .catch((error) => console.error(error));
+    }
+  };
+
+  //päivitetään customer
+  const updateCustomer = (updatedCustomer) => {
+    fetch(updatedCustomer._links.customer.href, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(updatedCustomer),
+    })
+      .then((repsonse) => {
+        if (repsonse.ok) {
+          setSnackMessage("The customer was updated successfully!");
+          setOpenSnackBar(true);
+          getCustomers();
+        } else {
+          window.alert("Something went wrong with saving");
+        }
+      })
+      .catch((error) => console.error(error));
+  };
+
+  //tallenetaan uusi customer
+  const handleSave = (newCustomer) => {
+    fetch(
+      "https://customer-rest-service-frontend-personaltrainer.2.rahtiapp.fi/api/customers",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
         },
-        //cellrenderer edit napille
-        {
-            headerName: '',
-            cellRenderer: params => (
-                <EditCustomer customer={params.data} onSave={updateCustomer} onCancel={handleCancel} />
-            ),
-            width: 120
+        body: JSON.stringify(newCustomer),
+      }
+    )
+      .then((response) => {
+        if (response.ok) {
+          setSnackMessage("The customer was saved successfully!");
+          setOpenSnackBar(true);
+        } else {
+          window.alert("Something went wrong with saving");
         }
-    ];
-
-    useEffect(() => {
         getCustomers();
-    }, []);
+      })
+      .catch((error) => console.error(error));
+  };
 
-    //haetaan kaikki customerit, fetch
-    const getCustomers = () => {
-        fetch("https://customerrestservice-personaltraining.rahtiapp.fi/api/customers")
-            .then(response => response.json())
-            .then(responseData => {
-                setCustomers(responseData._embedded.customers);
-            })
-            .catch(error => console.error(error));
-    };
+  const handleCancel = () => {};
 
-    //poistetaan customer
-    const deleteCustomer = (customer) => {
-        if (window.confirm("Are you sure you want to delete this customer?")) {
-            fetch(customer._links.customer.href, { method: 'DELETE' })
-                .then(response => {
-                    if (response.ok) {
-                        setSnackMessage("The customer was deleted succesfully!");
-                        setOpenSnackBar(true);
-                        getCustomers();
-                    } else {
-                        window.alert("Something went wrong with deleting");
-                    }
-                })
-                .catch(error => console.error(error));
-        }
-    };
+  return (
+    <>
+      <AddCustomer onSave={handleSave} onCancel={handleCancel} />
+      <div
+        className="ag-theme-material"
+        style={{ height: 600, width: 1500, margin: "auto" }}
+      >
+        <AgGridReact
+          rowData={customers}
+          columnDefs={columnDefs}
+          pagination={true}
+          paginationPageSize={15}
+        />
+      </div>
+      <Snackbar
+        open={openSnackbar}
+        autoHideDuration={3000}
+        onClose={() => setOpenSnackBar(false)}
+        message={snackmessage}
+      />
 
-    //päivitetään customer
-    const updateCustomer = (updatedCustomer) => {
-        fetch(updatedCustomer._links.customer.href, {
-            method: 'PUT',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(updatedCustomer)
-        })
-            .then(repsonse => {
-                if (repsonse.ok) {
-                    setSnackMessage("The customer was updated successfully!");
-                    setOpenSnackBar(true);
-                    getCustomers();
-                } else {
-                    window.alert("Something went wrong with saving");
-                }
-            })
-            .catch(error => console.error(error));
-    };
-
-    //tallenetaan uusi customer
-    const handleSave = (newCustomer) => {
-        fetch("https://customerrestservice-personaltraining.rahtiapp.fi/api/customers", {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(newCustomer)
-        })
-            .then(response => {
-                if (response.ok) {
-                    setSnackMessage("The customer was saved successfully!");
-                    setOpenSnackBar(true);
-                } else {
-                    window.alert("Something went wrong with saving");
-                }
-            })
-            .catch(error => console.error(error));
-    };
-
-    const handleCancel = () => {
-
-    };
-
-    return (
-        <>
-
-            <AddCustomer onSave={handleSave} onCancel={handleCancel} />
-            <div className="ag-theme-material" style={{ height: 600, width: 1700, margin: 'auto' }} >
-
-                <AgGridReact
-                    rowData={customers}
-                    columnDefs={columnDefs}
-                    pagination={true}
-                    paginationPageSize={15}
-
-                />
-            </div>
-            <Snackbar
-                open={openSnackbar}
-                autoHideDuration={3000}
-                onClose={() => setOpenSnackBar(false)}
-                message={snackmessage}
-
-            />
-
-            <CsvDownloader
-                datas={customers}
-                text="Export CSV"
-                filename={'customerdata' + new Date().toLocaleString()}
-                extension=".csv"
-                className="btn-btn-success"
-                style={{ color: 'white' }}
-            />
-        </>
-    );
+      <CsvDownloader
+        datas={customers}
+        text="Export CSV"
+        filename={"customerdata" + new Date().toLocaleString()}
+        extension=".csv"
+        className="btn-btn-success"
+        style={{ color: "white" }}
+      />
+    </>
+  );
 }
--- src/components/Training.jsx
+++ src/components/Training.jsx
@@ -2,8 +2,8 @@
 
 import { useEffect, useState } from "react";
 import { AgGridReact } from "ag-grid-react";
-import 'ag-grid-community/styles/ag-grid.css';
-import 'ag-grid-community/styles/ag-theme-material.css';
+import "ag-grid-community/styles/ag-grid.css";
+import "ag-grid-community/styles/ag-theme-material.css";
 import { format } from "date-fns";
 import { Button, Snackbar } from "@mui/material";
 import AddTraining from "./AddTraining";
@@ -11,168 +11,188 @@ import AddTraining from "./AddTraining";
 import { Link } from "react-router-dom";
 
 export default function Training() {
-    //States
-    const [trainings, setTrainings] = useState([]);
-    const [openSnackbar, setOpenSnackBar] = useState(false);
-    const [snackmessage, setSnackMessage] = useState("");
-    const [customers, setCustomers] = useState([]);
+  //States
+  const [trainings, setTrainings] = useState([]);
+  const [openSnackbar, setOpenSnackBar] = useState(false);
+  const [snackmessage, setSnackMessage] = useState("");
+  const [customers, setCustomers] = useState([]);
 
-    // Kentät
-    const columnDefs = [
-        {
-            field: 'date',
-            valueFormatter: params => format(new Date(params.value), 'dd.MM.yyyy HH:mm'),
-            sortable: true,
-            filter: true,
-            floatingFilter: true
-        },
-        { field: 'duration', sortable: true, filter: true, floatingFilter: true },
-        { field: 'activity', sortable: true, filter: true, floatingFilter: true },
-        {
-            headerName: 'Customer',
-            valueGetter: params => {
-                const customer = params.data.customer;
-                return `${customer.firstname} ${customer.lastname}`;
-            },
-            sortable: true,
-            filter: true,
-            floatingFilter: true
-        },
-        {
-            headerName: '',
-            cellRenderer: params => (
-                <Button
-                    size="small"
-                    color="error"
-                    onClick={() => deleteTraining(params.data)}
-                >
-                    Delete
-                </Button>
-            ),
-            width: 120
-        },
-    ];
+  // Kentät
+  const columnDefs = [
+    {
+      field: "date",
+      valueFormatter: (params) =>
+        format(new Date(params.value), "dd.MM.yyyy HH:mm"),
+      sortable: true,
+      filter: true,
+      floatingFilter: true,
+    },
+    { field: "duration", sortable: true, filter: true, floatingFilter: true },
+    { field: "activity", sortable: true, filter: true, floatingFilter: true },
+    {
+      headerName: "Customer",
+      valueGetter: (params) => {
+        const customer = params.data.customer;
+        return `${customer.firstname} ${customer.lastname}`;
+      },
+      sortable: true,
+      filter: true,
+      floatingFilter: true,
+    },
+    {
+      headerName: "",
+      cellRenderer: (params) => (
+        <Button
+          size="small"
+          color="error"
+          onClick={() => deleteTraining(params.data)}
+        >
+          Delete
+        </Button>
+      ),
+      width: 120,
+    },
+  ];
 
-    // Haetaan harjoitukset ja asiakastiedot
-    useEffect(() => {
-        getTrainings();
-        getCustomers();
-    }, []);
+  // Haetaan harjoitukset ja asiakastiedot
+  useEffect(() => {
+    getTrainings();
+    getCustomers();
+  }, []);
 
-    // Haetaan kaikki harjoitukset
-    const getTrainings = () => {
-        fetch("https://customerrestservice-personaltraining.rahtiapp.fi/gettrainings")
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error('Failed to fetch trainings');
-                }
-                return response.json();
-            })
-            .then(responseData => {
-                setTrainings(responseData);
-            })
-            .catch(error => console.error(error));
-    };
-
-    // Haetaan kaikki asiakastiedot
-    //get all customers
-    const getCustomers = () => {
-        fetch("https://customerrestservice-personaltraining.rahtiapp.fi/api/customers")
-            .then(response => response.json())
-            .then(responseData => {
-                setCustomers(responseData._embedded.customers);
-            })
-            .catch(error => console.error(error));
-    };
-
-    // Harjoituksen poistaminen
-    const deleteTraining = (training) => {
-        if (window.confirm("Are you sure you want to delete this training?")) {
-            fetch(`https://customerrestservice-personaltraining.rahtiapp.fi/api/trainings/${training.id}`, {
-                method: 'DELETE',
-                headers: {
-                    'Content-Type': 'application/json'
-                }
-            })
-                .then(response => {
-                    if (!response.ok) {
-                        throw new Error(`Failed to delete training: ${response.status}`);
-                    }
-                    setSnackMessage("The training was deleted successfully!");
-                    setOpenSnackBar(true);
-                    getTrainings();
-                })
-                .catch(error => {
-                    console.error(error);
-                    window.alert("Something went wrong with deleting");
-                });
+  // Haetaan kaikki harjoitukset
+  const getTrainings = () => {
+    fetch(
+      "https://customer-rest-service-frontend-personaltrainer.2.rahtiapp.fi/api/gettrainings"
+    )
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Failed to fetch trainings");
         }
-    };
+        return response.json();
+      })
+      .then((responseData) => {
+        setTrainings(responseData);
+      })
+      .catch((error) => console.error(error));
+  };
 
-    // tallenetaan uusi treeni: ei toimi
-    const handleSave = () => {
+  // Haetaan kaikki asiakastiedot
+  //get all customers
+  const getCustomers = () => {
+    fetch(
+      "https://customer-rest-service-frontend-personaltrainer.2.rahtiapp.fi/api/customers"
+    )
+      .then((response) => response.json())
+      .then((responseData) => {
+        setCustomers(responseData._embedded.customers);
+      })
+      .catch((error) => console.error(error));
+  };
 
-        const selectedCustomer = customers.find(customer => customer.firstname === trainings.firstname && customer.lastname === trainings.lastname);
-
-        if (!selectedCustomer) {
-            window.alert("Customer not found!");
-            return;
+  // Harjoituksen poistaminen
+  const deleteTraining = (training) => {
+    if (window.confirm("Are you sure you want to delete this training?")) {
+      fetch(
+        `https://customerrestservice-personaltraining.rahtiapp.fi/api/trainings/${training.id}`,
+        {
+          method: "DELETE",
+          headers: {
+            "Content-Type": "application/json",
+          },
         }
-
-        const isoDate = new Date(trainings.date).toISOString();
-
-        const trainingToAdd = {
-            date: isoDate,
-            activity: trainings.activity,
-            duration: trainings.duration,
-            customer: selectedCustomer
-        };
-
-        fetch("https://customerrestservice-personaltraining.rahtiapp.fi/api/trainings", {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(trainingToAdd)
+      )
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error(`Failed to delete training: ${response.status}`);
+          }
+          setSnackMessage("The training was deleted successfully!");
+          setOpenSnackBar(true);
+          getTrainings();
         })
-            .then(response => {
-                if (response.ok) {
-                    setSnackMessage("The training was saved successfully!");
-                    setOpenSnackBar(true);
-                    getTrainings();
-                } else {
-                    window.alert("Something went wrong with saving");
-                }
-            })
-            .catch(error => {
-                console.error(error);
-                window.alert("An error occurred while processing the request");
-            });
-    };
+        .catch((error) => {
+          console.error(error);
+          window.alert("Something went wrong with deleting");
+        });
+    }
+  };
 
-
-    const handleCancel = () => {
-
-    };
-
-    return (
-        <>
-            <AddTraining customers={customers} onSave={handleSave} onCancel={handleCancel} trainings={trainings} />
-            <div className="ag-theme-material" style={{ height: 600, width: 1000, margin: 'auto' }}>
-                <AgGridReact
-                    rowData={trainings}
-                    columnDefs={columnDefs}
-                    pagination={true}
-                    paginationPageSize={9}
-                />
-            </div>
-            <Snackbar
-                open={openSnackbar}
-                autoHideDuration={3000}
-                onClose={() => setOpenSnackBar(false)}
-                message={snackmessage}
-            />
-            <Link to="/statistics">Go to Statistics</Link>
-        </>
+  // tallenetaan uusi treeni: ei toimi
+  const handleSave = () => {
+    const selectedCustomer = customers.find(
+      (customer) =>
+        customer.firstname === trainings.firstname &&
+        customer.lastname === trainings.lastname
     );
+
+    if (!selectedCustomer) {
+      window.alert("Customer not found!");
+      return;
+    }
+
+    const isoDate = new Date(trainings.date).toISOString();
+
+    const trainingToAdd = {
+      date: isoDate,
+      activity: trainings.activity,
+      duration: trainings.duration,
+      customer: selectedCustomer,
+    };
+
+    fetch(
+      "https://customerrestservice-personaltraining.rahtiapp.fi/api/trainings",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(trainingToAdd),
+      }
+    )
+      .then((response) => {
+        if (response.ok) {
+          setSnackMessage("The training was saved successfully!");
+          setOpenSnackBar(true);
+          getTrainings();
+        } else {
+          window.alert("Something went wrong with saving");
+        }
+        getTrainings();
+      })
+      .catch((error) => {
+        console.error(error);
+        window.alert("An error occurred while processing the request");
+      });
+  };
+
+  const handleCancel = () => {};
+
+  return (
+    <>
+      <AddTraining
+        customers={customers}
+        onSave={handleSave}
+        onCancel={handleCancel}
+        trainings={trainings}
+      />
+      <div
+        className="ag-theme-material"
+        style={{ height: 600, width: 1000, margin: "auto" }}
+      >
+        <AgGridReact
+          rowData={trainings}
+          columnDefs={columnDefs}
+          pagination={true}
+          paginationPageSize={9}
+        />
+      </div>
+      <Snackbar
+        open={openSnackbar}
+        autoHideDuration={3000}
+        onClose={() => setOpenSnackBar(false)}
+        message={snackmessage}
+      />
+      <Link to="/statistics">Go to Statistics</Link>
+    </>
+  );
 }
